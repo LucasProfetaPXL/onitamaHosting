e220d2fab72b46d8f6ce36cb0b1c88454fe64db0
SiebeDuijstersPXL <siebe.duijsters@student.pxl.be>
2024-04-19 15:58:02 +0200

    login.js

--- Frontend/js/login.js
+++ Frontend/js/login.js
@@ -1,27 +1,46 @@
-document.addEventListener('login', Function()){ //TODO login moet veranderd worden door de login-knop naam
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById("loginForm").addEventListener('submit', function (event) {
+        event.preventDefault();
 
+        var emailInput = document.getElementById('emailInput');
+        var passwordInput = document.getElementById('passwordInput');
+        //validate input
+        if (!emailInput.value || !passwordInput.value) { //TODO temporary check -> @lucasProfeta
+            window.alert("Fill in email and password before logging in");
+            throw new Error("Login fault"); //exit function
+        }
 
-}
-fetch('https://localhost:5051/api/Authentication/token', {
-	method: 'POST',
-	headers: {
-		//?
-	},
-	body: JSON, stringify({ document.getElementById('email'), document.getElementById('password')}) // TODO email & password moeten veranderd worden in de variabelen van index.html
-	.then(response => response.json())
-	.then(response => {
-		if (response.readyState == XMLHttpRequest.DONE && response.status == 200){
-			window.Location = '../lobby.html';
-		}
-		else{
-			throw new Error('Foute logingegevens')
-		}
-	}
-	.catch(Error => {
-		document.getElementById('password').innerHTML == Error.message; // TODO password vervangen door id van passwordfield
-	}
-}
+        const formData = new FormData(this);
 
+        // Convert form data to JSON object
+        const data = {};
+        formData.forEach(function (value, key) {
+            data[key] = value;
+        });
 
+        // fetch naar backand
+        fetch('https://{host}:{port}/api/Authentication/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        .then(response => {
+            if (!response.ok) {
+                window.alert("Error, network fault");
+                throw new Error("Login incorrect; network fault"); //exit function
+            }
+            return response.json();
+        })
+        .then(data => {
+            window.alert('succesfull login' + data);
+            //TODO what to do when succesfull login?
 
-//TODO: check als velden (email & password) zijn ingevuld
+        })
+        .catch(error => {
+            //TODO errorhandling
+            window.alert('Your account is not registered yet, please make an account first.' + error);
+        });
+    });
+});
