f00dc0fcf93a24f6e75e74394e1d04d6539e1f1d
Artur.Tkachenko@student.pxl.be <Artur.Tkachenko@student.pxl.be>
2024-04-22 16:20:03 +0200

    modal_change

--- Frontend/js/modal.js
+++ Frontend/js/modal.js
@@ -2,6 +2,8 @@ const modalOpenBtns = document.querySelectorAll('.modal_btn');
 const loginBackdrop = document.querySelector('#login_backdrop');
 const registerBackdrop = document.querySelector('#register_backdrop');
 const modalCloseBtns = document.querySelectorAll('.close_button');
+const loginBtnToRegister = document.querySelector('.register_button_login');
+const registerBtnToLogin = document.querySelector('.login_button_register');
 
 modalOpenBtns.forEach(button => {
     button.addEventListener('click', function(e) {
@@ -27,4 +29,14 @@ modalCloseBtns.forEach(button => {
             registerBackdrop.classList.remove('modal_open');
         }
     });
+});
+
+loginBtnToRegister.addEventListener('click', function() {
+    loginBackdrop.classList.remove('modal_open');
+    registerBackdrop.classList.add('modal_open');
+});
+
+registerBtnToLogin.addEventListener('click', function() {
+    registerBackdrop.classList.remove('modal_open');
+    loginBackdrop.classList.add('modal_open');
 });
