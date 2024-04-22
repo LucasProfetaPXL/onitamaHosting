21040ce8ba3fa64ab0257feb66ad5d9b748dec73
LucasProfetaPXL <12301302@student.pxl.be>
2024-04-22 14:44:23 +0200

    nog eens iets

--- Frontend/js/register.js
+++ Frontend/js/register.js
@@ -17,49 +17,50 @@ function validateRegister(){
     //Making a preventDefault(), this is because some methods have default inputs. This prevents that we get default inputs.
     form.addEventListener("submit", (e) => {
             e.preventDefault();
-            validateEmailInput(email.value);
-            validatePassword(password, repeat_Password);
+            //validateEmailInput(email.value);
+            validatePassword(password.value, repeat_Password.value);
         }
     )
     email.addEventListener("change", (e) => {
         setTimeout(() => validator.isEmail(), 2000);
     });
-
-    //Making fetch request for the reqister page
-    fetch('https://localhost:8080/api/Authentication/register',{
-        method: "POST",
-        mode: "cors",
-        headers: {
-            'content-type': 'application/json'
-        },
-        //here we are going to make a json file, because we do the fetch with a json file
-        body: JSON.stringify(
-            {
-                username: userName.value,
-                email: email.value,
-                password: password.value
+    if (validatePassword(password, repeat_Password)) {
+        //Making fetch request for the reqister page
+        fetch('https://localhost:8080/api/Authentication/register', {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    'content-type': 'application/json'
+                },
+                //here we are going to make a json file, because we do the fetch with a json file
+                body: JSON.stringify(
+                    {
+                        username: userName.value,
+                        email: email.value,
+                        password: password.value
+                    }
+                )
             }
         )
-        }
-
-
-        )
-
-
-
+            .then(response =>{
+            if (!response.ok){
+                alert("het bestaat al")
+            }
+            })
+    }
 }
 function validatePassword(password, repeat_Password){
     //Here we are first going to look if the passwords are equal to eachother
-    if (password.value.trim() !== repeat_Password.value.trim()) {
+    if (password.trim() !== repeat_Password.trim()) {
         alert("Passwords do not match.");
+        return false;
+    }
+    else{
+        return  true;
     }
     //Now we're going to
 }
-function validateEmailInput(email) {
-    if (!validator.isEmail(email)) {
-        alert("Invalid email address.");
-    }
-}
+
 
 function validateUsername(username){
 
