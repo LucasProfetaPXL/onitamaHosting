67441eccaf04b14ce74d2055b68f4ae650b05f33
LucasProfetaPXL <12301302@student.pxl.be>
2024-05-03 13:01:04 +0200

    Leave table.js is afgemaakt

---
+++ Frontend/js/Leave_Table.js
@@ -0,0 +1,33 @@
+document.getElementById('leave-button').addEventListener('click', function (event){
+    const tableid = localStorage.getItem('tableId');
+    const sessionID = localStorage.getItem('sessionID');
+    fetch(` https://localhost:5051/api/Tables/${tableid}/leave`,{
+        method: 'POST',
+        mode: 'cors',
+        headers:{
+            'Authorization': `Bearer ${sessionID}`
+        }
+    })
+        .then(response =>{
+            if (!response.ok){
+                throw new Error("error")
+            }
+            return response.json()
+        })
+        .then(data =>{
+            console.log(data)
+            if (data.status === 404){
+                console.log("This table is not found")
+            }
+            if (data.status === 200){
+                console.log("You just left succesfully")
+                setTimeout(function (){
+                    window.location.href = '../html/lobby.html'
+                }, 1000)
+            }
+            else{
+                console.log(data.status);
+                console.log(data.json())
+            }
+        })
+})
