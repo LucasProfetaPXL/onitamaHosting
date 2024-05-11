30e85fab4ce0b670afc404287753cf28067d897f
LucasProfetaPXL <12301302@student.pxl.be>
2024-05-11 08:48:35 +0200

    Alle testen van gamfactory werken eindelijk na 34567876 uur van me leven er aan te verspillen

--- Backend/Onitama.Core/GameAggregate/GameFactory.cs
+++ Backend/Onitama.Core/GameAggregate/GameFactory.cs
@@ -10,6 +10,8 @@ using Onitama.Core.SchoolAggregate;
 using Onitama.Core.TableAggregate;
 using Onitama.Core.TableAggregate.Contracts;
 using Onitama.Core.UserAggregate;
+using Onitama.Core.Util;
+using Onitama.Core.Util.Contracts;
 
 namespace Onitama.Core.GameAggregate;
 
@@ -25,22 +27,65 @@ internal class GameFactory : IGameFactory
         _moveCardRepository = moveCardRepository;
     }
 
+    private MoveCardGridCellType[,] GenerateRandomGrid()
+    {
+        Random random = new Random();
+        MoveCardGridCellType[,] grid = new MoveCardGridCellType[5, 5];
+
+        for (int x = 0; x < 5; x++)
+        {
+            for (int y = 0; y < 5; y++)
+            {
+                grid[x, y] = (MoveCardGridCellType)random.Next(0, 3);
+            }
+        }
+
+        return grid;
+    }
+
+    private List<MoveCard> GeneratePossibleMoveCards()
+    {
+        List<MoveCard> possibleMoveCards = new List<MoveCard>();
+        for (int i = 0; i < 10; i++)
+        {
+            MoveCardGridCellType[,] grid = GenerateRandomGrid(); 
+            MoveCard moveCard = new MoveCard("MoveCard" + i, grid, Color.Black); 
+            possibleMoveCards.Add(moveCard);
+        }
+
+        return possibleMoveCards;
+    }
+
+
     public IGame CreateNewForTable(ITable table)
     {
-        IPlayer[] seatedPLayers = new IPlayer[2];
+        IPlayer[] seatedPlayers = new IPlayer[2];
         Color[] colors = new Color[2];
+        Random random = new Random();
+
         for (int i = 0; i < table.SeatedPlayers.Count; i++)
         {
             colors[i] = table.SeatedPlayers[i].Color;
-            seatedPLayers[i] = table.SeatedPlayers[i];
-            for (int j = 0; j < 2; j++)
+            seatedPlayers[i] = table.SeatedPlayers[i];
+
+            List<MoveCard> possibleMoveCards = GeneratePossibleMoveCards(); 
+
+            for (int j = 0; j < possibleMoveCards.Count - 1; j++)
             {
-                _moveCard = new MoveCard();
-                table.SeatedPlayers[i].MoveCards.Add(_moveCard);
+                int index = random.Next(j, possibleMoveCards.Count);
+                MoveCard temp = possibleMoveCards[j];
+                possibleMoveCards[j] = possibleMoveCards[index];
+                possibleMoveCards[index] = temp;
+            }
+            for (int k = 0; k < 2; k++)
+            {
+                MoveCard selectedMoveCard = possibleMoveCards[k];
+                table.SeatedPlayers[i].MoveCards.Add(selectedMoveCard);
             }
         }
-        _playMat = new PlayMat(seatedPLayers);
-        _game = new Game(table.Id, _playMat, seatedPLayers, _moveCard) ;
+        MoveCard extraMoveCard = GeneratePossibleMoveCards()[random.Next(0, 10)]; 
+        _playMat = new PlayMat(seatedPlayers);
+        _game = new Game(table.Id, _playMat, seatedPlayers, extraMoveCard); 
         _moveCardRepository.LoadSet(table.Preferences.MoveCardSet, colors);
         return _game;
     }
