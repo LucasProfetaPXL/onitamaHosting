f7c7109e104f9e143ec082137d3ec41ebb4b2461
SiebeDuijstersPXL <siebe.duijsters@student.pxl.be>
2024-05-13 11:46:01 +0200

    card placeholders

--- Frontend/js/MoveCards.js
+++ Frontend/js/MoveCards.js
@@ -1,29 +1,32 @@
 document.addEventListener('DOMContentLoaded', function (){
     const tabled = localStorage.getItem('tableId');
+    const sessionID = sessionStorage.getItem('sessionID');
     //const sessionID = sessionStorage.getItem('sessionID');
     fetch(`https://localhost:5051/api/Games/${tabled}`,{
         method : 'GET',
         mode : 'cors',
         headers: {
-            'Accept': 'text/plain'
+            // 'Accept': 'text/plain',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${sessionID}`
         }
 
     })
         .then(response =>{
             if (response.status === 200){
-                console.log("request werkte")
+                console.log("request werkte");
                 GetMoveCards(response);
             }
             if (response.status === 500){
-                console.log(response)
-                console.log(response.json())
+                console.log(response);
+                console.log(response.json());
             }
             else{
                 response.json().then(error =>{
-                    console.log(error.message)
+                    console.log(error.message);
                 })
-                console.log("There went something wrong")
-                console.log(response.message)
+                console.log("There went something wrong");
+                console.log(response.message);
             }
         })
 })
