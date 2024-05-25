3bdf58a81ce1d0cc0997669a67458a77857817a9
LucasProfetaPXL <12301302@student.pxl.be>
2024-05-25 13:01:27 +0200

    .

--- Frontend/js/fetchPossibleMoves.js
+++ Frontend/js/fetchPossibleMoves.js
@@ -1,7 +1,5 @@
 let selectPawnId;
 let selectCardName;
-
-// Function to fetch possible moves
 const fetchPossibleMoves = (pawnId, cardName) => {
     if (!pawnId || !cardName) {
         console.log("Both pawn and card need to be selected.");
@@ -10,8 +8,6 @@ const fetchPossibleMoves = (pawnId, cardName) => {
     const sessionID = sessionStorage.getItem('sessionID')
     const gameId = localStorage.getItem('tableId');
     const url = `http://localhost:5051/api/Games/${gameId}/possible-moves/${pawnId}/for-card/${cardName}`;
-
-    // Make the fetch request
     fetch(url, {
         method: 'GET',
         mode: 'cors',
@@ -37,35 +33,28 @@ const fetchPossibleMoves = (pawnId, cardName) => {
         })
         .catch(error => {
             console.error('Error fetching possible moves:', error);
-        });
-};
-
-// Function to handle pawn selection
+        })
+}
 const handlePawnSelection = (pawnId) => {
     selectPawnId = pawnId;
     console.log("Selected pawn:", selectPawnId);
     fetchPossibleMoves(selectPawnId, selectCardName);
-};
-
-// Function to handle card selection
+}
 const handleCardSelection = (cardName) => {
     selectCardName = cardName;
     console.log("Selected card:", selectCardName);
     fetchPossibleMoves(selectPawnId, selectCardName);
-};
-
-// Add event listeners for pawn and card selection
-// Example: Assuming pawn elements have class "pawn" and card elements have class "card"
+}
 const pawns = document.querySelectorAll('.pawn');
 pawns.forEach(pawn => {
     pawn.addEventListener('click', () => {
         handlePawnSelection(pawn.id);
-    });
-});
+    })
+})
 
 const cards = document.querySelectorAll('.cardholder');
 cards.forEach(card => {
     card.addEventListener('click', () => {
         handleCardSelection(card.dataset.cardName);
-    });
-});
+    })
+})
